64c091e10cb4dc974999c0aafd2ab5b9b262891b
ruru5697 <429611948@qq.com>
2026-04-28 21:05:13 +0800

    refactor: remove unused FastQAConfig

--- tests/qa/fast/unit/test_imports.py
+++ tests/qa/fast/unit/test_imports.py
@@ -7,7 +7,6 @@ def test_can_import_fast_qa_modules():
     fast_module = importlib.import_module("by_qa.qa.fast")
     assert hasattr(fast_module, "FastQAEngine")
 
-    assert importlib.import_module("by_qa.qa.fast.config")
     assert importlib.import_module("by_qa.qa.fast.engine")
     assert importlib.import_module("by_qa.qa.fast.graph")
     assert importlib.import_module("by_qa.qa.fast.nodes.retrieve")

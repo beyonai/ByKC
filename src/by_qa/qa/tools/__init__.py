8e2350da2a725c112aed1d53a286794dcb65d1db
ruru5697 <429611948@qq.com>
2026-05-21 11:00:34 +0800

    feat: export BaseOperation and KnowledgeSearchOperation from tools package

--- src/by_qa/qa/tools/__init__.py
+++ src/by_qa/qa/tools/__init__.py
@@ -4,8 +4,13 @@ from by_qa.qa.tools.knowledge_tools import (
     DispatcherToolMiddleware,
     ServiceToolDispatcher,
 )
+from by_qa.qa.tools.operations.base import BaseOperation, DispatchRequest
+from by_qa.qa.tools.operations.knowledge_search import KnowledgeSearchOperation
 
 __all__ = [
+    "BaseOperation",
     "DispatcherToolMiddleware",
+    "DispatchRequest",
+    "KnowledgeSearchOperation",
     "ServiceToolDispatcher",
 ]
